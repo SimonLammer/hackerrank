8d3ae84590500c8a87946586b9b024a6884842f9
Simon Lammer <lammer.simon@gmail.com>
2017-06-15 22:42:00 +0200

    Solve Maximum Draws

---
+++ Mathematics/01 Fundamentals/02 Maximum Draws/Maximum Draws.java
@@ -0,0 +1,15 @@
+// https://www.hackerrank.com/challenges/maximum-draws
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for (int t = 0; t < T; t++) {
+            int n = in.nextInt();
+            System.out.println(n + 1);
+        }
+    }
+}
